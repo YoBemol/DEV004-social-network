f9941e8102c175b0f9d220c5969cdc914c6b0c1e
Belen Molina <belen.molina.z@gmail.com>
2023-03-13 16:12:57 -0500

    primeras rutas

---
+++ src/components/home.js
@@ -0,0 +1,69 @@
+import { navigate } from "../router";
+export const Home = () => {
+
+    const homeSection = document.createElement('section');
+    homeSection.className = 'section'
+    
+    const homeSectionTitle = document.createElement('p');      
+    homeSectionTitle.id = 'comment1' 
+    homeSectionTitle.textContent = 'Eres nueva/o en INFO NATURA?';
+
+    const homeSectionSubTitle = document.createElement('p');      
+    homeSectionSubTitle.id = 'comment2' 
+    homeSectionSubTitle.textContent = 'Registrate';
+
+    //boton crear cuenta con google
+    const buttonGoogle = document.createElement('button');    
+    buttonGoogle.textContent = 'Inicia sesion con Google';
+
+    //boton crear cuenta con correo y contrasena 
+    const buttonRegister = document.createElement('button');
+    buttonRegister.textContent = 'Crear cuenta';    //Registrate
+
+    const line = document.createElement('hr');  
+
+    //formulario para loguearse
+    const formLogin = document.createElement('form');
+    formLogin.className = 'formlogin';
+
+    const textEmail = document.createElement('p');      
+    textEmail.id = 'textEmail' 
+    textEmail.textContent = 'Correo electronico';
+
+    const inputEmail = document.createElement('input');
+    inputEmail.type = 'email';
+    inputEmail.placeholder = 'usuario@email.com';
+
+    const textPassword = document.createElement('p');      
+    textPassword.id = 'textPassword' 
+    textPassword.textContent = 'Contrasena';
+
+    const inputPassword = document.createElement('input');
+    inputPassword.type = 'password';
+    inputPassword.placeholder = 'Tu contraseña';
+
+    const buttonLogin = document.createElement('button');    
+    buttonLogin.textContent = 'Ingresar';
+
+        
+           //Ingresar
+    /*const homeFooter = document.createElement('footer');
+    const homeFooterParagraph = document.createElement('p');
+    homeFooterParagraph.textContent = 'Botanico icono creado por hemannkumar - Flaticon';*/
+
+    buttonRegister.addEventListener('click', () => navigate('/register'));
+
+    homeSection.appendChild(homeSectionTitle);
+    homeSection.appendChild(homeSectionSubTitle);
+    homeSection.appendChild(buttonGoogle);
+    homeSection.appendChild(buttonRegister);
+    homeSection.appendChild(line);
+    homeSection.appendChild(formLogin);
+    homeSection.appendChild(textEmail);
+    homeSection.appendChild(inputEmail);
+    homeSection.appendChild(textPassword);
+    homeSection.appendChild(inputPassword);
+    homeSection.appendChild(buttonLogin);
+
+    return homeSection;
+};
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+const ROUTES = {};
+
+export const navigate = (pathname) => {
+    const path = typeof ROUTES[pathname] !== 'function' ? pathname : '/';
+    window.history.pushState({}, path, window.location.origin + pathname);
+    const root = document.getElementById('root');
+    root.innerHTML = '';
+    root.appendChild(ROUTES[pathname]());
+};
+
+export const addRoutes = (routes) => {
+    Object.keys(routes).reduce((accumulator, currentRoute) => {
+      // seria buena agregar validaciones
+      accumulator[currentRoute] = routes[currentRoute];
+      return accumulator;
+    }, ROUTES);
+  };
